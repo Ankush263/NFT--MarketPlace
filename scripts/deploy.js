38af37d858785ff801c6588e0ecdb5203a04effa
Ankush263 <ankushbanik263@gmail.com>
2022-08-16 10:01:41 +0530

    update deploy js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,27 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+const { ethers } = require("hardhat");
 const hre = require("hardhat");
+const fs = require("fs");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const [deployer] = await ethers.getSigners();
+  const balance = await deployer.getBalance();
+  const Marketplace = await hre.ethers.getContractFactory("NFTMarketplace");
+  const marketplace = await Marketplace.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  await marketplace.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const data = {
+    address: marketplace.address,
+    abi: JSON.parse(marketplace.interface.format('json'))
+  }
 
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  //This writes the ABI and address to the mktplace.json
+  fs.writeFileSync('./Marketplace.json', JSON.stringify(data))
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
