c4aaa9ee0f8b193dc2e6edcec48a6cbb7356b228
Ankush263 <ankushbanik263@gmail.com>
2022-08-16 09:56:15 +0530

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-ethers');
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.7",
+  paths: {
+    artifacts: './frontend/src/artifacts',
+  },
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
