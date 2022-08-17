350f46a6e2d9d3a37a5cad54a13863678f80b33a
Ankush263 <ankushbanik263@gmail.com>
2022-08-17 16:52:16 +0530

    update

--- frontend/src/components/NftCard.jsx
+++ frontend/src/components/NftCard.jsx
@@ -1,29 +1,35 @@
 import React from 'react';
 import '../styles/NftCard.css';
-import nft1 from '../images/nft1.png';
 import ethereum from '../images/ethereum.png';
 import { Link } from 'react-router-dom';
 
 
+
 const NftCard = (props) => {
+
+  const newTo = {
+    pathname: "/nfttitle/"+props.data.tokenId
+  }
+
+
   return (
-    <Link className='nftcard-link' to='/nfttitle'>
+    <Link className='nftcard-link' to={newTo}>
       <div className="nftcard">
         <div className="nftcard-box">
         <div className="nft-img-holder">
 
-          <img className='nftcard-img' src={nft1} />
+          <img className='nftcard-img' src={props.data.image} />
 
         </div>
         <div className="nft-data-holder">
           <div className="nft-data nft-data-name">
-            Name: Monkey
+            Name: {props.data.characterName}
           </div>
           <div className="nft-data nft-data-id">
-            Id:
+            Id: {props.data.tokenId}
           </div>
           <div className="nft-data nft-data-price">
-            Price: 0.80
+            Price: {props.data.price}
             <img className='small-logo' src={ethereum} />
           </div>
         </div>
